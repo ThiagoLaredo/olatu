e232afce6908e1f126d0e5845ead1371faf16c9c
thiago laredo <thiago@olatu.com.br>
2023-11-16 11:08:33 -0300

    inserção de depoimento

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import ScrollSuave from './modules/scroll-suave.js';
+import MenuMobile from './modules/menu-mobile.js';
+import ScrollAnima from './modules/scroll-anima.js';
+
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
+
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
