c10ce0c67cf780798a8f6a9eb623307c8bf389ad
thiago laredo <thiago@olatu.com.br>
2023-12-25 13:40:09 -0300

    refatoração geral

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -37,3 +37,5 @@ export default class MenuMobile {
     return this;
   }
 }
+
+
--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -2,10 +2,8 @@ export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
-
     this.checkDistance = this.checkDistance.bind(this);
   }
-
   // Pega a distância de cada item em relação
   // ao topo do site
   getDistance() {
@@ -22,7 +20,7 @@ export default class ScrollAnima {
   // em relação ao scroll do site
   checkDistance() {
     this.distance.forEach((item) => {
-      if (window.pageYOffset > item.offset) {
+      if (window.scrollY > item.offset) {
         item.element.classList.add('ativo');
       } else if (item.element.classList.contains('ativo')) {
         item.element.classList.remove('ativo');
@@ -44,6 +42,3 @@ export default class ScrollAnima {
     window.removeEventListener('scroll', this.checkDistance);
   }
 }
-
-
-
