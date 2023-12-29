6f4aca36e384f815b3355a0b863da107ccd51bc4
thiago laredo <thiago@olatu.com.br>
2023-12-29 15:03:18 -0300

    refatoração geral

--- js/projetos.js
+++ js/projetos.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function initializeSwiper() {
     var swiper = new Swiper(".mySwiper", {
       direction: "vertical",
+      speed: 1000,
       mousewheel: true,
       preventClicks: false,
       preventClicksPropagation: false,
@@ -13,33 +14,36 @@ document.addEventListener('DOMContentLoaded', function () {
         clickable: false,
       },
       on: {
-        // init: function () {
-        //   this.pagination.el.style.display = 'none';
-        //   hideProjectMenu();
-        // },
+        init: function () {
+          this.pagination.el.style.display = 'none';
+          hideProjectMenu();
+        },
         slideChange: function () {
           let currentSlideIndex = this.realIndex;
-
+      
           // Remove 'active' class from all menu items
           document.querySelectorAll('.project-menu-item').forEach(function (menuItem) {
-            menuItem.classList.remove('active');
+              menuItem.classList.remove('active');
           });
-
-          // Get the hash value of the current slide
-          let currentSlideHash = swiper.slides[currentSlideIndex].getAttribute('data-hash');
-
-          // Find the corresponding menu item and add 'active' class
-          let correspondingMenuItem = document.querySelector(`.project-menu-item[href="#${currentSlideHash}"]`);
-          if (correspondingMenuItem) {
-            correspondingMenuItem.classList.add('active');
+      
+          // Check if swiper is defined
+          if (swiper && swiper.slides && swiper.slides[currentSlideIndex]) {
+              // Get the hash value of the current slide
+              let currentSlideHash = swiper.slides[currentSlideIndex].getAttribute('data-hash');
+      
+              // Find the corresponding menu item and add 'active' class
+              let correspondingMenuItem = document.querySelector(`.project-menu-item[href="#${currentSlideHash}"]`);
+              if (correspondingMenuItem) {
+                  correspondingMenuItem.classList.add('active');
+              }
           }
-
-          // if (currentSlideIndex >= 1) {
-          //   this.pagination.el.style.display = 'flex';
-          // } else {
-          //   this.pagination.el.style.display = 'none';
-          // }
-        },
+      
+          if (currentSlideIndex >= 1) {
+              this.pagination.el.style.display = 'flex';
+          } else {
+              this.pagination.el.style.display = 'none';
+          }
+      },
       },
     });
 
@@ -52,6 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
         hidePagination();
         showProjectMenu();
       });
+
       menuLateral.addEventListener('mouseleave', function () {
         hideProjectMenu();
         showPagination();
@@ -71,28 +76,35 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function hideProjectMenu() {
       if (projectMenu) {
-        projectMenu.style.display = 'none';
+        projectMenu.classList.remove('show-element');
+        pagination.style.display = 'flex';
+
       }
     }
 
     function hidePagination() {
       if (pagination) {
-        pagination.style.display = 'none';
+        pagination.classList.remove('show-element');
       }
     }
 
     function showPagination() {
       if (pagination) {
-        pagination.style.display = 'flex';
+        pagination.classList.add('show-element');
+
       }
     }
 
     function showProjectMenu() {
       if (projectMenu) {
-        projectMenu.style.display = 'flex';
+        projectMenu.classList.add('show-element');
+        pagination.style.display = 'none';
+
       }
     }
   }
 
+
   initializeSwiper();
 });
+
